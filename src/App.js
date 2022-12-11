dd8ca4f2eb2e45d6f4421d187824fdaba3954dcd
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-12-11 19:59:10 +0300

    changed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the Feature branch
+          I was changed on the Feature
         </a>
       </header>
     </div>
